249529e58cdeab9029f0c395d53115035e628e02
mrakasondara <rakasondara21@gmail.com>
2026-05-29 18:50:36 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -7,8 +7,8 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-space-grotesk",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${spaceGrotesk.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+// import type { Config } from "tailwindcss";
+
+// const config: Config = {
+//   content: [],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         grotesk: ["var(--font-space-grotesk)"],
+//         poppins: ["var(--font-poppins)"],
+//       },
+//     },
+//   },
+// };
+
+// export default config;
