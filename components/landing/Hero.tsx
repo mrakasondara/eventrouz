2ba565489c9b294e094f2fc0ea85aef22d59f97a
mrakasondara <rakasondara21@gmail.com>
2026-06-14 18:54:31 +0700

    feat (ui) : add icon

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
       <div className="flex items-center md:items-start gap-y-10 md:gap-y-5 flex-col py-15 px-5 gap-x-5 w-full lg:w-3/4 lg:mx-auto bg-beige">
         <div className="flex w-full justify-between md:static lg:relative">
           <h2 className="text-[4rem] leading-20 text-center md:text-start md:leading-26 lg:leading-28 md:text-[5rem] lg:text-[6rem] w-full lg:w-3/4 font-chewy font-extrabold text-black text-shadow-lg text-shadow-white">
-            Tremendous Events Experience.
+            Temukan Event Spektakuler, Tanpa Ribet.
           </h2>
           <TicketCard />
         </div>
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -14,8 +14,15 @@ export const Header = () => {
         pathname === "/signin" || pathname === "/signup" ? "hidden" : "flex"
       } w-full py-3 px-5 border-b-4 items-center`}
     >
-      <div className="flex w-full lg:w-3/4 mx-auto">
-        <h1 className="text-2xl font-bold">Eventrouz</h1>
+      <div className="flex w-full items-center lg:w-3/4 mx-auto">
+        <div className="flex gap-1 items-center">
+          <img
+            src="/images/icon.png"
+            className="h-[40px] scale-200"
+            alt="icon"
+          />
+          <h1 className="text-xl font-bold">Eventrouz</h1>
+        </div>
         <div className="flex gap-2 ml-auto justify-end items-center">
           {pathname != "/" && (
             <Button variant="brutalism" size="icon-sm">
