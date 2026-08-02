1d5dbb8eb0e080383804222e4c139b279f47ba58
mrakasondara <rakasondara21@gmail.com>
2026-08-02 16:05:09 +0700

    refactor(event-filter): change filter trigger from onChange value into submit form

--- components/events/EventFilterSection.tsx
+++ components/events/EventFilterSection.tsx
@@ -30,54 +30,87 @@ export const EventFilterSection = ({
     searchParams.get("search")?.toString() ?? ""
   );
 
-  const currentStatus = searchParams.get("status")?.toString() ?? "";
+  const [status, setStatus] = useState<string | null>(
+    searchParams.get("status")?.toString() ?? ""
+  );
 
   useEffect(() => {
     setSearchTerm(searchParams.get("search")?.toString() ?? "");
+    setStatus(searchParams.get("status")?.toString() ?? "");
   }, [searchParams]);
 
-  const handleSearch = useDebouncedCallback((term: string) => {
-    setSearchTerm(term);
+  const onSubmitHandler = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("test");
+    updateSearchandFilter({ search: searchTerm, status });
+  };
 
+  const updateSearchandFilter = (
+    updates: Record<string, string | null | undefined>
+  ) => {
     const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set("search", term);
-    } else {
-      params.delete("search");
-    }
 
-    replace(`${pathname}?${params.toString()}`, { scroll: false });
-  }, 300);
-
-  const handleFilter = (term: string | null) => {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set("status", term.toLowerCase());
-    } else {
-      params.delete("status");
-    }
+    Object.entries(updates).forEach(([key, value]) => {
+      if (value) {
+        params.set(key, value.toLowerCase());
+      } else {
+        params.delete(key);
+      }
+    });
 
     replace(`${pathname}?${params.toString()}`, { scroll: false });
   };
 
+  // const handleSearch = (term: string) => {
+  //   setSearchTerm(term);
+
+  //   const params = new URLSearchParams(searchParams);
+  //   if (term) {
+  //     params.set("search", term);
+  //   } else {
+  //     params.delete("search");
+  //   }
+
+  //   replace(`${pathname}?${params.toString()}`, { scroll: false });
+  // };
+
+  // const handleFilter = (term: string | null) => {
+  //   const params = new URLSearchParams(searchParams);
+  //   if (term) {
+  //     params.set("status", term.toLowerCase());
+  //   } else {
+  //     params.delete("status");
+  //   }
+
+  //   replace(`${pathname}?${params.toString()}`, { scroll: false });
+  // };
+
   return (
     <section className="flex flex-col gap-8 mt-10 w-full">
-      <form className="flex gap-5 w-full">
+      <form className="flex gap-5 w-full" onSubmit={onSubmitHandler}>
         <InputGroup className="w-3/4 md:w-1/2">
           <InputGroupInput
             placeholder="Cari berdasarkan nama event..."
             className="font-grotesk"
             value={searchTerm}
-            onChange={(e) => handleSearch(e.target.value)}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <InputGroupAddon>
             <SearchIcon className="text-muted-foreground" />
           </InputGroupAddon>
-          <InputGroupAddon align="inline-end">
-            <InputGroupButton>Cari</InputGroupButton>
+          <InputGroupAddon align="inline-end" className="mx-2">
+            <InputGroupButton
+              className="border-2 border-black shadow-[2px_4px_2px_0px_#323232] cursor-pointer"
+              type="submit"
+            >
+              Cari
+            </InputGroupButton>
           </InputGroupAddon>
         </InputGroup>
-        <Select value={currentStatus} onValueChange={handleFilter}>
+        <Select
+          value={status ?? ""}
+          onValueChange={(value) => setStatus(value || null)}
+        >
           <SelectTrigger className="w-[250px] px-3">
             <SelectValue
               placeholder="Filter berdasarkan status"
