194bf0df4c8728e5af654fbe07569fd9587a3509
mrakasondara <rakasondara21@gmail.com>
2026-08-06 15:02:08 +0700

    feat : add middleware for protecting admin page

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,27 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth({
-  callbacks: {
-    authorized: ({ token }) => {
-      return !!token;
+export default withAuth(
+  function middleware(req) {
+    const token = req.nextauth.token;
+    const path = req.nextUrl.pathname;
+
+    if (path.startsWith("/admin") && token?.role !== "admin") {
+      return NextResponse.redirect(new URL("/events", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        return !!token;
+      },
     },
-  },
-  pages: {
-    signIn: "/signin",
-  },
-});
+    pages: {
+      signIn: "/signin",
+    },
+  }
+);
 
-export const config = { matcher: ["/profile/:path"] };
+export const config = {
+  matcher: ["/profile", "/profile/:path*", "/admin", "/admin/:path*"],
+};
