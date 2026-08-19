072bfe88184b039cc7c9a3f50705a33fca101247
mrakasondara <rakasondara21@gmail.com>
2026-08-19 20:51:49 +0700

    style: update some styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -31,7 +31,8 @@ const buttonVariants = cva(
         md: "h-10 gap-1.5 px-6 has-data-[icon=inline-end]:pr-4 has-data-[icon=inline-start]:pl-5 -translate-y-[3px] hover:translate-[4px] hover:shadow-none shadow-[4px_4px_0px_0px_#323232]",
         lg: "h-11 gap-1.5 px-8 has-data-[icon=inline-end]:pr-5 has-data-[icon=inline-start]:pl-5 -translate-y-[4px] hover:translate-[6px] hover:shadow-none shadow-[6px_6px_0px_0px_#323232]",
         icon: "size-10",
-        "icon-xs": "size-7 [&_svg:not([class*='size-'])]:size-3",
+        "icon-xs":
+          "size-7 [&_svg:not([class*='size-'])]:size-3 -translate-y-[1px] hover:translate-[2px] hover:shadow-none shadow-[2px_2px_0px_0px_#323232]",
         "icon-sm":
           "size-9 -translate-y-[1px] hover:translate-[3px] hover:shadow-none shadow-[3px_3px_0px_0px_#323232]",
         "icon-lg": "size-11",
